1abcd0101c02610637815ad4e363c2bb76aec367
abinanthan1 <abinanthan1011@gmail.com>
2025-04-30 03:01:16 +0530

    Added Sorting and Searching API functionality with the help of LinkQ

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,14 +10,20 @@ namespace API.Controllers
     {
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>>GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts(string? orderBy,
+        string? searchTerm)
         {
-             return await context.Products.ToListAsync();
+            var query = context.Products
+            .Sort(orderBy)
+            .Search(searchTerm)
+            .AsQueryable();
+
+            return await query.ToListAsync();
         }
 
 
         [HttpGet("{id}")] //api/products/2
-         public async Task<ActionResult<Product>> GetProduct (int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
             var product = await context.Products.FindAsync(id);
 
